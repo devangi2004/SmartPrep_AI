afcc0450b2302f75039c4a212f7ea74ecc7d2590
Devangi Inani <139806614+devangi2004@users.noreply.github.com>
2026-06-12 23:19:55 +0530

    Delete frontend directory

--- frontend/src/api/generator.js
+++
@@ -1,6 +0,0 @@
-import authApi from './auth';
-
-export const generateQuestions = async (text, type = 'mcq') => {
-  const response = await authApi.post('/generator', { text, type });
-  return response.data;
-};
